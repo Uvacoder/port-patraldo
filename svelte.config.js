c9a9c667a126b5a6a3aa59879efc36740af39284
christophe <cheftech@patraldo.com>
2022-08-16 17:40:30 -0500

    updating svelte to new +page routing

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,22 @@ import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
-  kit: {
-    adapter: adapter({
-      // default options are shown
-      out: "build",
-      precompress: false,
-      envPrefix: "APATCOM_",
-    }),
-  },
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			out: "build",
+			precompress: false,
+			envPrefix: "APATCOM_",
+		}),
+	},
 
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
 };
 export default config;
