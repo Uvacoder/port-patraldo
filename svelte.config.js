18fa1cbe6417778e25aed21c7e49d7e2f1b76841
Uvacoder <uva22940coder@gmail.com>
2022-10-16 11:00:19 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			out: "build",
-			precompress: false,
-			envPrefix: "APATCOM_",
-		}),
+		adapter: adapter()
 	},
 
 };
